19806d55fa7811266d3de21bed055b0a50e83177
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-08-24 18:31:16 +0100

    Router: disable lazy loading

--- src/router.js
+++ src/router.js
@@ -1,30 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Hello from './components/Hello.vue'
+import Error404 from './components/Error404.vue'
+
 Vue.use(VueRouter)
 
-function load (component) {
-  // '@' is aliased to src/components
-  return () => import(`@/${component}.vue`)
-}
-
 export default new VueRouter({
-  /*
-   * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
-   * it is only to be used only for websites.
-   *
-   * If you decide to go with "history" mode, please also open /config/index.js
-   * and set "build.publicPath" to something other than an empty string.
-   * Example: '/' instead of current ''
-   *
-   * If switching back to default "hash" mode, don't forget to set the
-   * build publicPath back to '' so Cordova builds work again.
-   */
-
   routes: [
-    { path: '/', component: load('Hello') },
+    { path: '/', component: Hello },
 
     // Always leave this last one
-    { path: '*', component: load('Error404') } // Not found
+    { path: '*', component: Error404 } // Not found
   ]
 })
