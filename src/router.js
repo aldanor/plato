7d1ead928624ea9c134e85b9f150479486629974
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-08-25 20:15:40 +0100

    Rename Hello.vue -> Main.vue

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Hello from './components/Hello.vue'
+import Main from './components/Main.vue'
 import Error404 from './components/Error404.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-    { path: '/', component: Hello },
+    { path: '/', component: Main },
 
     // Always leave this last one
     { path: '*', component: Error404 } // Not found
